f411f0a699653a02bf8b7f6f63ccd747d64ce53b
Nicola Bergo <nickbarybin@gmail.com>
2019-03-12 00:49:10 +0300

    some sample changes for travis

--- test/fizzbuzz_test.go
+++ test/fizzbuzz_test.go
@@ -10,24 +10,24 @@ import (
 
 // ExampleCorrectInput'demostrates the extra spaces and newlines tolerance of FizzBuzz()
 func ExampleCorrectInput () {
-	correctInput := "5 4 1 0  -6   -5 \n   50 13456789     \n\n 3 \n\n"
+	correctInput := "5 4 1 0  -6   -5 \n   50 9223372036854775807     \n\n 3 \n\n"
 
 	r := strings.NewReader(correctInput)
 	err := fizzbuzz.FizzBuzz(r, os.Stdout)
 
 	fmt.Printf(", ERROR = %+v", err)
-	// Output: buzz 4 1 fizzbuzz fizz buzz buzz 13456789 fizz , ERROR = <nil>
+	// Output: buzz 4 1 fizzbuzz fizz buzz buzz 9223372036854775807 fizz , ERROR = <nil>
 }
 
 // ExampleOverflowInput demonstrates detecting too large number on input
 func ExampleOverflowInput () {
-	overflowInput := "1 100 35 15 -666 12345678909887666565656554433455 1 23 17"
+	overflowInput := "1 100 35 15 -666 9223372036854775808 1 23 17"
 
 	r := strings.NewReader(overflowInput)
 	err := fizzbuzz.FizzBuzz(r, os.Stdout)
 
 	fmt.Printf(", ERROR = %+v", err)
-	// Output: 1 buzz buzz fizzbuzz fizz , ERROR = strconv.ParseInt: parsing "12345678909887666565656554433455": value out of range
+	// Output: 1 buzz buzz fizzbuzz fizz , ERROR = strconv.ParseInt: parsing "9223372036854775808": value out of range
 }
 
 // ExampleNoiIntInput demonstrates detecting non-int value on iput
